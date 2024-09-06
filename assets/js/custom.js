a889793603946f4f1c85e26126cd2c5692a86b22
urvishcirklestudio <urvishs.cirklesyudio@gmail.com>
2024-09-06 18:25:15 +0530

    hero banner

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,14 +12,17 @@ $(document).ready(function(){
         disableOnInteraction: true       
     });
 
+    $('header').css('--header-height',`${$('header').outerHeight()}px`)
+    // $(".bordered").css("border", "1px solid black");
+
     $('.nav-items > a').click(function(e){         
             $(this).parents('.nav-items').find('.inner-menu').addClass('open') 
             $(this).parents('.nav-items').find('.inner-menu[main-inner-open]').find('.main-back-arrow .nav-heading-name').text($(this).parents('.nav-items').attr('data-nav-name'));
     });
     
     $('.menu-icons').click(function(){   
-        $('.header-nav').addClass('nav-show')  
-        $(this).toggleClass('active')
+        $('.header-nav').toggleClass('nav-show')  
+        $('.menu-icons').toggleClass('active')
     });
     
     $('.mg-list-main .mg-list-heading').click(function(){            
@@ -33,13 +36,20 @@ $(document).ready(function(){
     $('.inner-back-arrow .icons').click(function(){       
         $(this).parents('.mg-links-wpr').removeClass('open')     
     });
-
+    $('.header-search-li .search-icons').click(function(){ 
+        $('.header-search').toggleClass('open')      
+    }); 
+    
     $(document).mouseup(function(e){ 
         if (!$(".header-nav nav").is(e.target) && $(".header-nav nav").has(e.target).length === 0){
             $(".header-nav").removeClass('nav-show');
             $(".menu-icons").removeClass('active');
         }
+        if (!$(".header-search-form-wpr").is(e.target) && $(".header-search-form-wpr").has(e.target).length === 0 && !$(".header-search-li").is(e.target) && $(".header-search-li").has(e.target).length === 0){
+            $(".header-search").removeClass('open'); 
+        }
     }); 
-
+    
+    
         
 })
